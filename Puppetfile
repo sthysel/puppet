24e8f42d8085a9b3c46344277994c63681fe9656
Thys Meintjes <tmeintjes@ccg.murdoch.edu.au>
2014-07-04 10:13:18 +0800

    Moved ccg to ccg's repo manifiest

--- Puppetfile
+++ Puppetfile
@@ -1,5 +1,11 @@
 forge "http://forge.puppetlabs.com"
 
+mod 'saz/timezone', "3.0.1"
 mod 'puppetlabs/stdlib'
 mod 'puppetlabs/apt'
-
+mod 'ccgcommon',
+  :git => "git://github.com/muccg/puppet-ccgcommon.git"
+  :ref => "1.0.2"
+mod 'repo',
+  :git => "git://github.com/muccg/puppet-repo.git"
+  :ref => "1.0.6"
--- manifests/thysdev.pp
+++ manifests/thysdev.pp
@@ -1,18 +1,29 @@
-include apt
-apt::ppa { 'ppa:webupd8team/java': }
+include ccgcommon
+include repo::ubuntuccg
 
+#apt::ppa { 'ppa:webupd8team/java': }
 
 Package { ensure => 'installed' }
 package {'oracle-java8-installer': }
 package {'oracle-java8-set-default': }
 
-$utilities = ['librarian-puppet', 'tmux', 'tree', 'git', 'vim', 'emacs']
-$pythondev = ['python-dev', 'python-virtualenv']
-$monitor = ['iotop', 'sysstat', 'iftop', 'htop', 'iptraf']
-package {$utilities: } 
-package {$pythondev: } 
-package {$monitor: } 
+$packages = ['librarian-puppet', 
+             'tmux', 
+             'tree', 
+             'git', 
+             'vim', 
+             'emacs', 
+             'mercurial', 
+             'mercurial-keyring',
+             'graphviz',
+             # python
+             'python-dev', 'python-virtualenv',
+             # monitoring
+             'iotop', 'sysstat', 'iftop', 'htop', 'iptraf',
+             'ccg'
+            ]
 
+package {$packages: } 
 
 user {'wortel':
     ensure => 'present',
